f4a1b65cf9d099d0dcb4d04985ae917f5b4c006f
charlesborius07 <2442832@cegeplimoilou.ca>
2026-05-14 15:14:16 -0400

    interface tactile amelioree

--- projet-fin-session/interface/mqtt_config.py
+++ projet-fin-session/interface/mqtt_config.py
@@ -1,6 +1,6 @@
 MQTT_CONFIG = {
     "broker": "mqtt.charlesborius07.com",
-    "port": 1883,
+    "port": 443,
     "username": "telecom",
     "password": "Teladmin1$",
     "device_id": "poste-05"
